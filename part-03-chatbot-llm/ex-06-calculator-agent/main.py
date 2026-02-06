78bc3b1876593f4b39b3e7564d5bc2690d41a851
anagabrielaoliveira <anagabrielaolvr@gmail.com>
2026-02-05 23:17:17 -0300

    remove SystemMessage do histórico e corrige importação

--- part-03-chatbot-llm/ex-06-calculator-agent/main.py
+++ part-03-chatbot-llm/ex-06-calculator-agent/main.py
@@ -9,40 +9,51 @@ load_dotenv()
 os.environ["OPENAI_API_KEY"]
 
 # ----------- Teste iterativo ----------- #
-
 def chat():
     estado = {
         "mensagens": [
-            SystemMessage(content="Você é um assistente prestativo e amigável.")
+            SystemMessage(content="Você é um assistente com nacionalidade nipônica, só vai responder em japonês")
+            #SystemMessage(content="Você é um assistente impaciente, todas suas respostas sao agressivas.")
         ],
         "resposta_atual": "",
         "contexto": "Usuário está aprendendo sobre AI"
     }
-    
+
+    # Carrego historico
+    try: 
+        with open("historico.json", "r", encoding="utf-8") as arquivo:
+            historico_carregado = json.load(arquivo)
+            estado['mensagens'] += messages_from_dict(historico_carregado) 
+
+    # Caso seja a primeira interacao com o agente
+    except:
+        pass
+
     print("Chatbot iniciado! Digite 'sair' para encerrar.\n")
     
     while True:
         user_input = input("Você: ")
         if user_input.lower() == "sair":
             
-            # estado['mensagens'] é uma lista de objetos da classe BaseMessage
+            # estado['mensagens'] e uma lista de objetos da classe BaseMessage
             # (AIMessage, HumanMessage, SystemMessage e ToolMessage)
             # para salvar essas infos no formato json utiliza-se o metodo
             # messages_to_dict para converter esses objetos em serializaveis, 
             # nesse caso um dicionário
-            historico_salvo_dict = messages_to_dict(estado['mensagens'])
             
+            # print('historico:', estado['mensagens'])
+
+            # Nao adiciono SystemMessage ao historico, para que eu possa mudar o comportamento
+            # do meu agente quando eu quiser
+            mensagens_filtradas = [ msg for msg in estado['mensagens'] if not isinstance(msg, SystemMessage)]
+
+            historico_salvo_dict = messages_to_dict(mensagens_filtradas)
+
             # Salva historico
             with open("historico.json", "w", encoding="utf-8") as arquivo:
                 json.dump(historico_salvo_dict, arquivo, ensure_ascii=False, indent=4)
 
             break
-        
-        # Carrega historico
-        with open("historico.json", "r", encoding="utf-8") as arquivo:
-            historico_carregado = json.load(arquivo)
-
-            estado['mensagens'] = messages_from_dict(historico_carregado)
         
         estado["mensagens"].append(HumanMessage(content=user_input))
         resultado = app.invoke(estado)  
