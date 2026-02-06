ff1cd4372d7d7b692e050125ebf2cddcae77b541
anagabrielaoliveira <anagabrielaolvr@gmail.com>
2026-02-06 16:42:11 -0300

    metadados removidos do historico salvo em JSON

--- part-03-chatbot-llm/ex-06-calculator-agent/main.py
+++ part-03-chatbot-llm/ex-06-calculator-agent/main.py
@@ -1,4 +1,4 @@
-from langchain_core.messages import HumanMessage, SystemMessage, messages_to_dict, messages_from_dict
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 from graph import app
 import os
 import json
@@ -12,8 +12,8 @@ os.environ["OPENAI_API_KEY"]
 def chat():
     estado = {
         "mensagens": [
-            SystemMessage(content="Você é um assistente com nacionalidade nipônica, só vai responder em japonês")
-            #SystemMessage(content="Você é um assistente impaciente, todas suas respostas sao agressivas.")
+            #SystemMessage(content="Você é um assistente com nacionalidade nipônica, só vai responder em japonês")
+            SystemMessage(content="Você é um assistente impaciente, todas suas respostas sao agressivas.")
         ],
         "resposta_atual": "",
         "contexto": "Usuário está aprendendo sobre AI"
@@ -23,7 +23,12 @@ def chat():
     try: 
         with open("historico.json", "r", encoding="utf-8") as arquivo:
             historico_carregado = json.load(arquivo)
-            estado['mensagens'] += messages_from_dict(historico_carregado) 
+            
+            for msg in historico_carregado:
+                if msg['role'] == 'user':
+                    estado['mensagens'].append(HumanMessage(content=msg['content']))
+                elif msg['role'] == 'assistant':
+                    estado['mensagens'].append(AIMessage(content=msg['content']))
 
     # Caso seja a primeira interacao com o agente
     except:
@@ -45,13 +50,23 @@ def chat():
 
             # Nao adiciono SystemMessage ao historico, para que eu possa mudar o comportamento
             # do meu agente quando eu quiser
-            mensagens_filtradas = [ msg for msg in estado['mensagens'] if not isinstance(msg, SystemMessage)]
+                        
+            historico_filtrado = []
 
-            historico_salvo_dict = messages_to_dict(mensagens_filtradas)
+            for msg in estado['mensagens']:
+                if not isinstance(msg, SystemMessage):
+                    role = "user" if msg.type == "human" else "assistant"
+
+                    conteudo = msg.content
+
+                    historico_filtrado.append({
+                        "role": role,
+                        "content": conteudo
+                        })
 
             # Salva historico
             with open("historico.json", "w", encoding="utf-8") as arquivo:
-                json.dump(historico_salvo_dict, arquivo, ensure_ascii=False, indent=4)
+                json.dump(historico_filtrado, arquivo, ensure_ascii=False, indent=4)
 
             break
         
